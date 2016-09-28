00f85dae14bdfcd315e51e1fe08de22d4026f37b
ivanmilano <ivan@mivaco.com>
2016-09-28 22:52:11 +0200

    Initial commit

---
+++ FagdagDotNet/Task1/ShippingOptions.cs
@@ -0,0 +1,9 @@
+﻿namespace FagdagDotNet.Task1
+{
+    public enum ShippingOptions
+    {
+        UPS = 100,
+        FedEx = 200,
+        Schenker = 300,
+    }
+}
---
+++ FagdagDotNet/Task2/TradeProcessor.cs
@@ -0,0 +1,105 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.SqlClient;
+using System.IO;
+
+namespace FagdagDotNet.Task2
+{
+    public class TradeProcessor
+    {
+        public void ProcessTrades(Stream stream)
+        {
+            var lines = new List<string>();
+            using (var reader = new StreamReader(stream))
+            {
+                string line;
+                while ((line = reader.ReadLine()) != null)
+                {
+                    lines.Add(line);
+                }
+            }
+
+            var trades = new List<TradeRecord>();
+
+            var lineCount = 1;
+            foreach (var line in lines)
+            {
+                var fields = line.Split(',');
+
+                if (fields.Length != 3)
+                {
+                    Console.WriteLine("WARN: Line {0} malformed. Only {1} field(s) found", lineCount, fields.Length);
+                    continue;
+                }
+
+                if (fields[0].Length != 6)
+                {
+                    Console.WriteLine("WARN: Trade currencies on line {0} malformed: '{1}'", lineCount, fields[0]);
+                    continue;
+                }
+
+                int tradeAmount;
+                if (!int.TryParse(fields[1], out tradeAmount))
+                {
+                    Console.WriteLine("WARN: Trade amount on line {0} is not a valid integer: '{1}'", lineCount, fields[1]);
+                }
+
+                decimal tradePrice;
+                if (!decimal.TryParse(fields[2], out tradePrice))
+                {
+                    Console.WriteLine("WARN: Trade price on line {0} is not a valid decimal: '{1}'", lineCount, fields[2]);
+                }
+                var sourceCurrencyCode = fields[0].Substring(0, 3);
+                var destinationCurrencyCode = fields[0].Substring(3, 3);
+
+                var trade = new TradeRecord
+                {
+                    SourceCurrency = sourceCurrencyCode,
+                    DestinationCurrency = destinationCurrencyCode,
+                    Lots = tradeAmount/LotSize,
+                    Price = tradePrice
+                };
+
+                trades.Add(trade);
+
+                lineCount ++;
+            }
+
+            using (var connection = new SqlConnection("ConnectionString"))
+            {
+                connection.Open();
+                using (var transacton = connection.BeginTransaction())
+                {
+                    foreach (var trade in trades)
+                    {
+                        var command = connection.CreateCommand();
+                        command.Transaction = transacton;
+                        command.CommandType = CommandType.StoredProcedure;
+                        command.CommandText = "dbo.insert_trade";
+                        command.Parameters.AddWithValue("@sourceCurrency", trade.SourceCurrency);
+                        command.Parameters.AddWithValue("@destinationCurrency", trade.DestinationCurrency);
+                        command.Parameters.AddWithValue("@lots", trade.Lots);
+                        command.Parameters.AddWithValue("@price", trade.Price);
+
+                        command.ExecuteNonQuery();
+                    }
+                    transacton.Commit();
+                }
+                connection.Close();
+            }
+
+            Console.WriteLine("INFO: {0} trades processed", trades.Count);
+        }
+
+        private static float LotSize = 100000f;
+    }
+
+    internal class TradeRecord
+    {
+        public string SourceCurrency { get; set; }
+        public string DestinationCurrency { get; set; }
+        public float Lots { get; set; }
+        public decimal Price { get; set; }
+    }
+}
